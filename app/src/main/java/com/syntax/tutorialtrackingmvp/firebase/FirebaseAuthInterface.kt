42912809abd829bc7950462177fecde64299548e
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-08-12 17:16:06 +0700

    starter project

--- app/src/main/java/com/syntax/tutorialtrackingmvp/firebase/FirebaseAuthInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.syntax.tutorialtrackingmvp.firebase
-
-interface FirebaseAuthInterface {
-
-    fun doSignUp(name: String, email: String, password: String, isSuccess: (Boolean) -> Unit)
-    fun doSignIn(email: String, password: String, isSuccess: (Boolean) -> Unit)
-}
